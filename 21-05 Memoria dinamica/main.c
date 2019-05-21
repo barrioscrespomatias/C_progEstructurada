f56b868c9439d4bebd9b57ccdcd287906d55f4cf
matiascrespo <40859037+matiascrespo@users.noreply.github.com>
2019-05-21 10:02:21 -0300

    Memoria dinamica

---
+++ 21-05 Memoria dinamica/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int* pNumero;
+//    int* pNumero2;
+
+
+    ///
+    pNumero = (int*) malloc(sizeof(int));
+    *pNumero = 99;
+    printf("heap->  pNumero: %d\n", pNumero);   /// Direccion de memoria que apunta a heap (?)
+    printf("stack-> &pNumero: %d\n", &pNumero); /// Direccion de memoria que esta en el stack (es el puntero original)
+
+    /// SI MALLOC NO ENCUENTRA ESPACIO EN MOMERIA DEVUELVE NULL
+    if(pNumero != NULL)
+    {
+        printf("valor -> *pNumero: %d\n", *pNumero); /// contenido dentro de direccion de memoria
+    }
+
+    printf("\n");
+
+    free(pNumero);
+
+    printf("Despues de hacer el free: \n\n");
+
+    printf("heap->  pNumero: %d\n", pNumero);   /// Direccion de memoria que apunta a heap (?)
+    printf("stack-> &pNumero: %d\n", &pNumero); /// Direccion de memoria heap
+    if(pNumero != NULL)
+    {
+        printf("valor-> *pNumero: %d\n", *pNumero); /// contenido dentro de direccion de memoria
+    }
+//    /// CALLOC ADEMAS DE PEDIR ESPACIO INICIALIZA EN CERO
+//    pNumero2 = (int*) calloc(sizeof(int), 1);
+//
+//     if(pNumero2 != NULL)
+//    {
+//        printf("*pNumero: %d\n", *pNumero2); /// contenido dentro de direccion de memoria
+//    }
+
+
+
+    return 0;
+}
